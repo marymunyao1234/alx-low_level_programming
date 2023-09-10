678454550011ba34e66479eaeefaefa05d37b138
marymunyao1234 <mary@eride.co.ke>
2023-09-10 00:58:06 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - 4-puts.c
  *
- * Return: Always 0 (success)
+ * Return: always 0
  */
+
 int main(void)
-{      puts("Programming is like building a multilingual puzzle\n"):
-       return (0):
-}	       
+{      
+	puts("\"Programming is like building a multilingual puzzle"):
+	return (0):	
+}	
