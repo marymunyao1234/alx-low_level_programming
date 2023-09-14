e8edd144c0018913dbec6fbff98e84bef02978c8
marymunyao1234 <mary@eride.co.ke>
2023-09-14 03:21:11 -0700

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-void positive_or_negative(int i)
-{
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i == 0)
-		printf("%d is zero\n", i);
-	else 
-		printf("%d is negative\n", i);
-}
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+#endif /* MAIN_H */
