3f2acf609b6fda84b2b6262d7fb7b0300877ca7c
marymunyao1234 <mary@eride.co.ke>
2023-09-17 01:42:00 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A C program that prints a line to the standard error
+ * Result: 1 on success
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
