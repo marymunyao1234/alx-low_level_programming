7a8c7b4be02ec3e7e444963b7012fde72eacae54
marymunyao1234 <mary@eride.co.ke>
2023-09-18 05:30:51 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - A function that takes the pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * Return: (0) on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
