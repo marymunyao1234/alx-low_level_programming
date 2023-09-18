b9d1daec499c621e0c5729f82d291f0064fea19e
marymunyao1234 <mary@eride.co.ke>
2023-09-18 11:47:53 -0700

    A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,22 @@
 #include "main.h"
 /**
- * 
- * _puts - function name
- * @str: function parameter
+ * rev_string - function name
+ * @s: function parameter
  */
-
-void puts2(char *str)
+void rev_string(char *s)
 {
 int i;
+int count = 0;
 
-for (i = 0; str[i] != '\0'; i++)
-_putchar (str[i]);
-_putchar ('\n');
+for (i = 0; s[i] != '\0'; i++)
+	count++;
+for (i = 0; i < count /2; i++)
+{
+char j;
+j = s[i];
+	s[i] = s[count - 1 - i];
+	s[count -1 -i] = j;
+}
+
+
 }
