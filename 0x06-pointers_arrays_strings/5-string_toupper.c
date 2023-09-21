16c6e83cafec9269629463b0406d9c0bb4c76043
marymunyao1234 <mary@eride.co.ke>
2023-09-21 11:18:30 -0700

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @str: string
+ * @*: string parameter
+ * Return: 0
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
